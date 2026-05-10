20bc2816a6ddf310711e69a94c75fd74f182d0b0
Tanvee Rajput <rajput2006tanvi@gmail.com>
2026-05-10 12:36:41 +0530

    fixed case-sensitive wrapAsync import

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const User=require("../models/user.js");
-const wrapAsync=require("../utils/wrapAsync");
+const wrapasync=require("../utils/wrapasync.js");
 const passport=require("passport");
 const {saveRedirectUrl}=require("../middleware.js");
  const usercontroller=require("../controller/users.js");
