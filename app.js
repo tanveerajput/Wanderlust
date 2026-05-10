52a0475c3d930ad8bd39432782ed4a617fca7b0f
Tanvee Rajput <rajput2006tanvi@gmail.com>
2026-05-10 13:20:07 +0530

    fixed mongo store config

--- app.js
+++ app.js
@@ -119,9 +119,15 @@ app.use((err, req, res, next) => {
 
 });
 
-app.listen(8080,()=>{
-    console.log("server is listening to port 8080");
-});  
+// app.listen(8080,()=>{
+//     console.log("server is listening to port 8080");
+// });
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
 
 
 
